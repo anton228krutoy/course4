f049d844efbaf7fd6c3fcaf08811250b78a4b2a9
anton228krutoy <moiseyevanton666@gmail.com>
2026-03-22 11:15:04 +0300

    Finally aligned the directions of magnetization vectors for the test image by high-quality background processing

--- course4_py/processing.py
+++ course4_py/processing.py
@@ -1,75 +1,3 @@
-# #!/usr/bin/env python3
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-# # Загружаем данные
-# data = np.load('dataset_roi.npz')
-# table = data['table_xy_g1_g2']
-
-# def show_max_xy(table):
-#     """Показывает максимумы x и y"""
-#     x = table[:, 0]
-#     y = table[:, 1]
-    
-#     print("\n" + "="*40)
-#     print("Максимум")
-#     print("="*40)
-#     print(f"Максимум x: {x.max()}")
-#     print(f"Максимум y: {y.max()}")
-#     print()
-
-
-# def plot_v(table):
-    
-#     x_raw = table[:, 0]
-#     y_raw = table[:, 1]
-#     vx_raw = table[:, 2].copy()
-#     vy_raw = table[:, 3].copy()
-
-#     for i in range(len(vx_raw)):
-#         if vx_raw[i] < 127:
-#             vx_raw[i] = vx_raw[i] * (-1)
-
-
-#     for i in range(len(vy_raw)):
-#         if vy_raw[i] < 127:
-#             vy_raw[i] = vy_raw[i] * (-1)
-    
-
-#     step = 15
-#     x = x_raw[::step]
-#     y = y_raw[::step]
-#     vx = vx_raw[::step] / 100
-#     vy = vy_raw[::step] /100
-
-
-#     # СОЗДАЕМ ГРАФИК
-#     plt.figure(figsize=(12, 10))
-
-#     # Рисуем векторы
-#     plt.quiver(x, y, vx, vy, 
-#                color='red', 
-#                scale=50,  # регулирует длину стрелок
-#                width=0.002,
-#                alpha=0.7)
-    
-#     plt.title('Векторное поле намагниченности')
-#     plt.xlabel('x, пиксели')
-#     plt.ylabel('y, пиксели')
-#     plt.gca().set_aspect('equal')
-#     plt.grid(True, alpha=0.3)
-#     plt.tight_layout()
-#     plt.savefig('vector_field_processed.png', dpi=150)
-#     plt.show()
-    
-#     print(f"Построено {len(x)} векторов")
-#     print("График сохранен в vector_field_processed.png")
-
-
-# plot_v(table)
-
-# data.close()
-
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
@@ -77,6 +5,19 @@ import matplotlib.pyplot as plt
 # Загружаем данные
 data = np.load('dataset_roi.npz')
 table = data['table_xy_g1_g2']
+
+def bg_v(v, N):
+    ''' Функция вычисляет самый фон картины с учетом выбросов 
+    '''
+
+    sorted_v = np.sort(v)
+    
+    min_x = sorted_v[N]
+    max_x = sorted_v[-1 - N]
+
+    bg = (max_x + min_x) / 2.0
+    
+    return bg
 
 def show_max_xy(table):
     """Показывает максимумы x и y"""
@@ -97,22 +38,18 @@ def plot_v(table):
     y_raw = table[:, 1]
     vx_raw = table[:, 2].copy()
     vy_raw = table[:, 3].copy()
-
-    for i in range(len(vx_raw)):
-        if vx_raw[i] < 100:
-            vx_raw[i] = vx_raw[i] * (-1)
-
-
-    for i in range(len(vy_raw)):
-        if vy_raw[i] < 100:
-            vy_raw[i] = vy_raw[i] * (-1)
+    vx_bg = bg_v(vx_raw, 9)
+    vy_bg = bg_v(vy_raw, 9)
+      
+    vx_raw = vx_raw - vx_bg
     
+    vy_raw = vy_raw - vy_bg
 
     step = 15
     x = x_raw[::step]
     y = y_raw[::step]
-    vx = vx_raw[::step] / 100
-    vy = vy_raw[::step] /100
+    vx = vx_raw[::step] / 80 
+    vy = vy_raw[::step] / 80 
 
 
     # СОЗДАЕМ ГРАФИК
